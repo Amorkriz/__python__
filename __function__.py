cffd52cad8a4d42a2680913a389770ee2dc6db0a
Amor_krzi <Amorkriz@users.noreply.github.com>
2019-11-15 16:30:40 +0800

    Update __function__.py

--- __function__.py
+++ __function__.py
@@ -210,10 +210,33 @@ def transfer(words):
             B[getN_lines(_)].append(mB)
     return R,G,B
 
-def uint2int(words):
-    temp = words[1:10]
-    if words[0] == '1':
-        m = -int(temp,2)
+# def uint2int(words):
+#     temp = words[1:10]
+#     if words[0] == '1':
+#         m = -int(temp,2)
+#     else:
+#         m = int(temp,2)
+#     return m
+
+def add_1(binary_inpute):#二进制编码加1
+    _,out = bin(int(binary_inpute,2)+1).split("b")
+    return out
+
+def reverse(binary_inpute):#取反操作
+    binary_inpute = list(binary_inpute)
+    binary_out = binary_inpute
+    for epoch,i in enumerate(binary_out):
+        if i == "0":
+            binary_out[epoch] = "1"
+        else:
+            binary_out[epoch] = "0"
+    return "".join(binary_out)
+
+def uint2int(a):
+    if a[0] == "1":#判断为负数
+        a_reverse = reverse(a[1:])  # 取反
+        a_add_1 = add_1(a_reverse)  # 二进制加1
+        a_int = -int(a_add_1, 2)
     else:
-        m = int(temp,2)
-    return m
+        a_int = int(a[1:])
+    return a_int
